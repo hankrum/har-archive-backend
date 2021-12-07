2e54e2273c0690d0e102f5664304409c11525572
hankrum42 <stefan.kolev.dantchev@gmail.com>
2021-12-07 16:33:43 +0200

    di

--- Har.Archive.Backend.Api/Startup.cs
+++ Har.Archive.Backend.Api/Startup.cs
@@ -1,21 +1,47 @@
+using Har.Archive.Backend.Data;
+using Har.Archive.Backend.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Har.Archive.Backend.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            this.RegisterData(services);
+            this.RegisterServices(services);
+        }
+
+        private void RegisterData(IServiceCollection services)
+        {
+            services.AddDbContext<MsSqlDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("HarFilesContext"),
+                b => b.MigrationsAssembly("Har.Archive.Backend.Api")));
+
+            services.BuildServiceProvider().GetService<MsSqlDbContext>().Database.Migrate();
+
+            services.AddScoped(typeof(IEfRepository<>), typeof(EFRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IHarFileService, HarFileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +52,15 @@ namespace Har.Archive.Backend.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
