79ee1a750a3ac862514f5a4b1488560532a2a20a
hankrum42 <stefan.kolev.dantchev@gmail.com>
2021-12-10 10:51:12 +0200

    unit tests

---
+++ Har.Archive.Backend.Api.Data.Services.Tests/Services/HarFileServiceTests/AllByPath_Should.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Har.Archive.Backend.Data;
+using Har.Archive.Backend.Data.Domain;
+using Har.Archive.Backend.Data.Services.Services;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Har.Archive.Backend.Api.Data.Services.Tests.Services.HarFileServiceTests
+{
+    public class AllByPath_Should
+    {
+        private readonly Mock<IUnitOfWork> unitOfWorkMock;
+        private readonly Mock<IPathService> pathServiceMock;
+        private readonly Mock<IMapper> mapperMock;
+        private readonly Fixture fixture;
+
+        public AllByPath_Should()
+        {
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            pathServiceMock = new Mock<IPathService>();
+            mapperMock = new Mock<IMapper>();
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public async Task ReturnProperData()
+        {
+            var path = fixture.Create<string>();
+
+            var pathEntry = fixture.Build<Path>()
+                .With(p => p.Text, path)
+                .Create();
+
+            var harFile = fixture.Build<HarFile>()
+                .With(h => h.Path, pathEntry)
+                .Create();
+
+            var repositoryMock = new Mock<IEfRepository<HarFile>>();
+            var expectedRepository = fixture.Create<IEfRepository<HarFile>>();
+            expectedRepository.Add(harFile);
+
+            // unitOfWorkMock.Setup(u => u.HarFiles).Returns(expectedQuery);
+        }
+    }
+}
---
+++ Har.Archive.Backend.Api.Data.Services.Tests/Services/HarFileServiceTests/Constructor_Should.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Har.Archive.Backend.Data;
+using Har.Archive.Backend.Data.Services.Services;
+using Moq;
+using System;
+using Xunit;
+
+namespace Har.Archive.Backend.Api.Data.Services.Tests.Services.HarFileServiceTests
+{
+    public class Constructor_Should
+    {
+        private readonly Mock<IUnitOfWork> unitOfWorkMock;
+        private readonly Mock<IPathService> pathServiceMock;
+        private readonly Mock<IMapper> mapperMock;
+
+        public Constructor_Should()
+        {
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            pathServiceMock = new Mock<IPathService>();
+            mapperMock = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public void ThrowNullArgumentException_WhenUnitOfWorkIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HarFileService(null, pathServiceMock.Object, mapperMock.Object));
+        }
+
+        [Fact]
+        public void ThrowNullArgumentException_WhenPathServiceIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HarFileService(unitOfWorkMock.Object, null, mapperMock.Object));
+        }
+
+        [Fact]
+        public void ThrowNullArgumentException_WhenMapperIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HarFileService(unitOfWorkMock.Object, pathServiceMock.Object, null));
+        }
+    }
+}
